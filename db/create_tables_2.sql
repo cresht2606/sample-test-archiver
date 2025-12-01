52b85f7fbec8eac8f1ec376d8a877d6c2fa0e3d9
Tran Minh Duc <tmduc2606@gmail.com>
2025-12-01 23:30:27 +0700

    Add changelog sample text

--- db/create_tables_2.sql
+++ db/create_tables_2.sql
@@ -68,3 +68,21 @@ INSERT INTO tests (subject_id, university, year, semester, type, drive_embed_url
 (3, 'MIT', 2024, 'Spring', 'Midterm', 'https://drive.google.com/file/d/1S9JsVG5XHT7fPvQVFSsc__5xbwXNNH_C/view?usp=sharing'),
 (3, 'MIT', 2024, 'Spring', 'Final', 'https://drive.google.com/file/d/1S9JsVG5XHT7fPvQVFSsc__5xbwXNNH_C/view?usp=sharing');
 
+-- Sample Changelog
+INSERT INTO changelog (version, date, body) VALUES
+('1.3.8', '2020-06-04',
+'Updated to Bootstrap v4.5
+Updated to jQuery v3.5.1
+Updated Material Icons v5.3.45
+Added RTL version
+Added Dark mode
+Added Dark RTL
+Fixed responsive issues'),
+('1.3.0', '2020-05-28',
+'Updated Material Icons
+Added RTL Version
+Added Dark Version
+Added Dark RTL Version
+Added 6 Color Schemes'),
+('1.0.0', '2020-03-04',
+'Initial Release');
