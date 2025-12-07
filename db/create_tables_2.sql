6cae26c519885da57244a408c3caf7d2b1e06d1c
Tran Minh Duc <tmduc2606@gmail.com>
2025-12-07 14:57:47 +0700

    Add Feedback tables to the SQL script
    - Feedback Table
    - Rating Table
    - Feedback Export Table

--- db/create_tables_2.sql
+++ db/create_tables_2.sql
@@ -28,6 +28,8 @@ CREATE TABLE tests (
     -- Constraint On FK
     CONSTRAINT fk_subject FOREIGN KEY (subject_id) REFERENCES subjects(id) ON DELETE CASCADE -- delete all tests if subject is feleted
 );
+-- Add the views column
+    ALTER TABLE tests ADD COLUMN views INT DEFAULT 0;
 
 -- Changelog entries
 CREATE TABLE IF NOT EXISTS changelog(
@@ -36,6 +38,33 @@ CREATE TABLE IF NOT EXISTS changelog(
     date DATE,
     body TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Feedback Table
+CREATE TABLE feedback (
+	id SERIAL PRIMARY KEY,
+    user_fullname VARCHAR(120),
+    email VARCHAR(120),
+    dob DATE,
+    problem_type VARCHAR(50) CHECK (problem_type IN ("website", "sample_test")),
+    test_id INT REFERENCES tests(id) ON DELETE SET NULL,
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Rating Table
+CREATE TABLE test_ratings (
+	test_id INT PRIMARY KEY REFERENCES tests(id),
+	avg_rating DECIMAL (3, 2) DEFAULT 0,
+	total_reviews INT DEFAULT 0
+);
+
+-- Feedback Export Table
+CREATE TABLE feedback_export_queue (
+	id SERIAL PRIMARY KEY,
+    feedback_id INT REFERENCES feedback(id),
+    exported BOOLEAN DEFAULT FALSE
 );
 
 -- Useful indexes
@@ -68,3 +97,21 @@ INSERT INTO tests (subject_id, university, year, semester, type, drive_embed_url
 (3, 'MIT', 2024, 'Spring', 'Midterm', 'https://drive.google.com/file/d/1S9JsVG5XHT7fPvQVFSsc__5xbwXNNH_C/view?usp=sharing'),
 (3, 'MIT', 2024, 'Spring', 'Final', 'https://drive.google.com/file/d/1S9JsVG5XHT7fPvQVFSsc__5xbwXNNH_C/view?usp=sharing');
 
+-- Sample Changelog
+INSERT INTO changelog (version, date, body) VALUES
+('1.3.8', '2020-06-04',
+'Updated to Bootstrap v4.5
+Updated to jQuery v3.5.1
+Updated Material Icons v5.3.45
+Added RTL version
+Added Dark mode
+Added Dark RTL
+Fixed responsive issues'),
+('1.3.0', '2020-05-28',
+'Updated Material Icons
+Added RTL Version
+Added Dark Version
+Added Dark RTL Version
+Added 6 Color Schemes'),
+('1.0.0', '2020-03-04',
+'Initial Release');
