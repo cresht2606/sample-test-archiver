a2663cebec88ebecd3ae6682d1e516923b80e4eb
Tran Minh Duc <tmduc2606@gmail.com>
2025-12-20 12:18:47 +0700

    Adjust database parameter connection

--- server/utils/db.js
+++ server/utils/db.js
@@ -6,11 +6,11 @@ require('dotenv').config();
 
 // Database connection (pool)
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    port: process.env.MYSQL_PORT || 3306,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 20,
     charset: "utf8mb4"
